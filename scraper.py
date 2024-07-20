a1419f5dec1402c195b2b19a912086385fbd83c7
Pranav Narahari <naraharipranav@gmail.com>
2024-07-19 23:04:43 -0500

    can run multiple bots at once

--- scraper.py
+++ scraper.py
@@ -4,31 +4,37 @@ from selenium.webdriver.common.keys import Keys
 import pyperclip
 import time 
 
-# Step 1: Set up the WebDriver
-driver = webdriver.Chrome()  # or `webdriver.Firefox()`, etc.
-driver1 = webdriver.Chrome()
-driver.get('https://www.epsilon-ai.com/search')
+genes = ["HLA-B27",  "ADRB3"] # "DAX1 (NR0B1)", "WNT10A", "TCF7L2", "BRCA1", "BRCA2", "SHANK3", "HLA-C", "CHRM2", "TSEN54",]
+# gene_talents = ["CHRM2", "TSEN54", "ADRB3", ]
+drivers = []
+for gene in genes:
+    drivers.append(webdriver.Chrome())
+
+for driver in drivers:
+    driver.get('https://www.epsilon-ai.com/search')
 
 time.sleep(3)
-# Step 2: Extract data
-# Wait for elements to load if necessary
-search_bar = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[2]/div/div[2]/div[1]/div[2]/textarea")
-search_text = "What 5 genes genetically increase your chances for obesity the most"
-search_bar.send_keys(search_text)
 
-search_bar.send_keys(Keys.RETURN)
+for i in range(0, len(genes)):
+    search_bar = drivers[i].find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[2]/div/div[2]/div[1]/div[2]/textarea")
+    search_text = f'what conditions are associated with {genes[i]}'
+    search_bar.send_keys(search_text)
+    search_bar.send_keys(Keys.RETURN)
+
 time.sleep(30)
-copy_button = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[2]/div/div[2]/div[3]/div[1]/div/div/div/div[1]/div[2]/button[1]")
-copy_button.click()
-time.sleep(1)
-copy_button_w_references = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[2]/div/div[2]/div[3]/div[1]/div/div/div/div[1]/div[2]/div/div/button[2]")
-copy_button_w_references.click()
+
+for driver in drivers:
+    copy_button = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[2]/div/div[2]/div[3]/div[1]/div/div/div/div[1]/div[2]/button[1]")
+    copy_button.click()
+
 time.sleep(1)
 
-copied_text = pyperclip.paste()
-print("Copied text:", copied_text)
-# time.sleep(5)
+for driver in drivers:
+    copy_button_w_references = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[2]/div/div[2]/div[3]/div[1]/div/div/div/div[1]/div[2]/div/div/button[2]")
+    copy_button_w_references.click()
+    time.sleep(1)
+    copied_text = pyperclip.paste()
+    print("Copied text:", copied_text)
 
-# Step 3: Close the WebDriver
-driver.quit()
-driver1.quit()
+for driver in drivers:
+    driver.quit()
